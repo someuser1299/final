aa876368c42867461c892e680416ac16389df76f
liad <liad@gmail.com>
2025-03-14 11:14:15 +0200

    attached more vars

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -8,61 +8,61 @@ resource "aws_key_pair" "liad_ssh_key_id" {
 }
 
 # VPC with DNS support
-resource "aws_vpc" "main" {
+resource "aws_vpc" "liad_main_vpc_id" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name = "main-vpc"
+    Name = var.main_vpc_name
   }
 }
 
 # Internet Gateway
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "liad_main_igw_id" {
+  vpc_id = aws_vpc.liad_main_vpc_id.id
 
   tags = {
-    Name = "main-igw"
+    Name = var.main_igw_name
   }
 }
 
 # Subnet
-resource "aws_subnet" "main_subnet" {
-  vpc_id                  = aws_vpc.main.id
+resource "aws_subnet" "liad_main_subnet_id" {
+  vpc_id                  = aws_vpc.liad_main_vpc_id.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = var.az
 
   tags = {
-    Name = "main-subnet"
+    Name = var.main_subnet_name
   }
 }
 
 # Route Table
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "liad_main_rt_id" {
+  vpc_id = aws_vpc.liad_main_vpc_id.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+    gateway_id = aws_internet_gateway.liad_main_igw_id.id
   }
 
   tags = {
-    Name = "main-route-table"
+    Name = var.main_rt_name
   }
 }
 
 # Route Table Association
 resource "aws_route_table_association" "main" {
-  subnet_id      = aws_subnet.main_subnet.id
-  route_table_id = aws_route_table.main.id
+  subnet_id      = aws_subnet.liad_main_subnet_id.id
+  route_table_id = aws_route_table.liad_main_rt_id.id
 }
 
 # Security Group
 resource "aws_security_group" "liad_security_group_id" {
   name        = var.security_group_name
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.liad_main_vpc_id.id
 
   # Inbound SSH
   ingress {
@@ -103,7 +103,7 @@ resource "aws_instance" "builder" {
   instance_type   = var.instance_type
   key_name        = aws_key_pair.liad_ssh_key_id.key_name
   vpc_security_group_ids = [aws_security_group.liad_security_group_id.id]
-  subnet_id       = aws_subnet.main_subnet.id
+  subnet_id       = aws_subnet.liad_main_subnet_id.id
 
   tags = {
     Name = "builder"
